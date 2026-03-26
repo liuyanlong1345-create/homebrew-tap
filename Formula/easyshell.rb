cb8c0d5513b9d44e6625125a22aaa716d610801c
liuliuyanlong <liuliuyanlong@example.com>
2026-03-27 00:32:25 +0800

    Fix repository URL to use correct username

--- Formula/easyshell.rb
+++ Formula/easyshell.rb
@@ -1,7 +1,7 @@
 class Easyshell < Formula
   desc "Command line tool for beginners - makes terminal operations easy and friendly"
-  homepage "https://github.com/liuliuyanlong/easy-shell"
-  url "https://github.com/liuliuyanlong/easy-shell/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/liuyanlong1345-create/easy-shell"
+  url "https://github.com/liuyanlong1345-create/easy-shell/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "f36bc336c1aa01b73689e9b68003fdb3c5179d8a6a0a897e1f44a3b82e9b8773"
   license "MIT"
 
