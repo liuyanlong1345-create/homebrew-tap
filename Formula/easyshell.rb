5b6effeaa38a96b529c1a492ae10e9ef479841e0
liuliuyanlong <liuliuyanlong@example.com>
2026-03-27 00:35:05 +0800

    Update SHA256 checksum

--- Formula/easyshell.rb
+++ Formula/easyshell.rb
@@ -2,7 +2,7 @@ class Easyshell < Formula
   desc "Command line tool for beginners - makes terminal operations easy and friendly"
   homepage "https://github.com/liuyanlong1345-create/easy-shell"
   url "https://github.com/liuyanlong1345-create/easy-shell/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f36bc336c1aa01b73689e9b68003fdb3c5179d8a6a0a897e1f44a3b82e9b8773"
+  sha256 "c5928fb286d64a6223205332d5a611e180d0c2c722f39cb39dfee67d42ed8585"
   license "MIT"
 
   depends_on "go" => :build
